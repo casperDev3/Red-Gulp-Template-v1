2165926a3708745da84bf2bac0abf529371e5b13
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-12 18:02:12 +0200

    add beta version gulp

---
+++ src/js/script.ts
@@ -0,0 +1,11 @@
+// import libs
+
+// import modules
+
+// import utils
+import { showAlert } from "./utils/showAlert"
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('hello')
+    showAlert('hello')
+})
